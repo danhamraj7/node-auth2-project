6789c086f115c44ad8dba87b2d2c47f04ae588cd
danhamraj <danhamraj@gmail.com>
2020-04-21 16:18:42 -0400

    completed auth/router/middleware env

--- api/api-router.js
+++ api/api-router.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 
-//authRouter = require("../auth/auth-router.js");
-//const usersRouter = require("../users/users-router.js");
-//const restricted = require("../auth/restricted-middleware.js");
+authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
+const restricted = require("../auth/restricted-middleware.js");
 
-//router.use("/auth", authRouter);
-//router.use("/users", restricted, usersRouter);
+router.use("/auth", authRouter);
+router.use("/users", restricted, usersRouter);
 
 router.get("/", (req, res) => {
   res.json({ api: "It's WORKING" });
--- api/server.js
+++ api/server.js
@@ -1,4 +1,4 @@
-//require("dotenv").config();
+require("dotenv").config();
 const express = require("express");
 
 const apiRouter = require("./api-router.js");
